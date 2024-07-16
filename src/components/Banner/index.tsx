5c983f3f235d8f97f078f7f0df5a8f14853ee57d
Santicabral0478 <scsantiagocabral@gmail.com>
2024-07-16 01:38:56 -0300

    update 13

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,19 @@
 "use client"
 
+import Image from "next/image";
+
 const Banner = () => {
 
 
   return (
     <section className="hero">
+      <div className="back-img-container">
+        <div className="space-color"></div>
+        <Image width={200} height={200} alt="background 1" src={"/mate.jpg"} ></Image>
+      </div>
     <div className="container">
 
       <div className="hero-content">
-
-        <p className="hero-subtitle">Filmlane</p>
 
         <h1 className="h1 hero-title">
           Unlimited <strong>Movie</strong>, TVs Shows, & More.
@@ -27,22 +31,6 @@ const Banner = () => {
             <a href="#">Romance,</a>
 
             <a href="#">Drama</a>
-          </div>
-
-          <div className="date-time">
-
-            <div>
-              {/* <ion-icon name="calendar-outline"></ion-icon> */}
-
-              <time>2022</time>
-            </div>
-
-            <div>
-              {/* <ion-icon name="time-outline"></ion-icon> */}
-
-              <time>128 min</time>
-            </div>
-
           </div>
 
         </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,112 +1,157 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.css"
+import { useEffect, useRef, useState } from "react";
+
 export const Header = ()=>{
+  const [isOcc, setIsOcc] : any = useState("");
+  const [isActive, setIsActive]: any = useState("");
+
+  const scrollTimeout: any = useRef(null);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+        setIsOcc("header-occ");
+
+
+      lastScrollY.current = currentScrollY;
+
+      // Clear the previous timeout
+      if (scrollTimeout.current) {
+        clearTimeout(scrollTimeout.current);
+      }
+
+      // Set a new timeout
+      scrollTimeout.current = setTimeout(() => {
+        setIsOcc("")
+      }, 1000); // 200ms after the user stops scrolling
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup function to remove the event listener and clear the timeout
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (scrollTimeout.current) {
+        clearTimeout(scrollTimeout.current);
+      }
+    };
+  }, []);
+
+  const handleOnClickActive = ()=>{
+    if(isActive === "active"){
+        setIsActive("")
+        document.body.style.overflow = 'auto';
+      }
+      else {
+        setIsActive("active")
+        document.body.style.overflow = 'hidden';
+      }
+  }
+
+  const handleOnCloseSideNavbar = ()=>{
+    setIsActive("");
+    document.body.style.overflow = 'auto';
+  }
+
     return(
-        <header className="header" data-header>
+        <header className={`header ${isOcc} header-${isActive}`} data-header>
         <div className="container">
     
-          <div className="overlay" data-overlay></div>
+          <div onClick={handleOnCloseSideNavbar} className={`overlay ${isActive}`} data-overlay></div>
     
-          <a href="./index.html" className="logo">
-            <img src="./assets/images/logo.svg" alt="Filmlane logo"/>
-          </a>
+          <Link className="width-logo" href={"/"}>
+            <Image className="header-logo" alt="Logo" width={300} height={100} src={"/logoheaderwidth.png"}></Image>
+          </Link>
+
+          <Link className="responsive-logo" href={"/"}>
+            <Image className="header-logo" alt="Logo" width={300} height={100} src={"/responsivelogo.png"}></Image>
+          </Link>
     
           <div className="header-actions">
     
             <button className="search-btn">
-              {/* <ion-icon name="search-outline"></ion-icon> */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
             </button>
     
-            <div className="lang-wrapper">
-              <label>
-                {/* <ion-icon name="globe-outline"></ion-icon> */}
-              </label>
-    
-              <select name="language" id="language">
-                <option value="en">EN</option>
-                <option value="au">AU</option>
-                <option value="ar">AR</option>
-                <option value="tu">TU</option>
-              </select>
-            </div>
-    
-            <button className="btn btn-primary">Sign in</button>
+            <Link className={styles.button} href="#">
+              <span className={styles.button__iconwrapper}>
+                  <svg width="10" className={styles.button__iconsvg} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                      <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                  </svg>
+                  
+                  <svg className={`${styles.button__iconsvg}  ${styles.button__iconsvgcopy}`} xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                      <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                  </svg>
+              </span>
+              Explore All
+            </Link>
     
           </div>
     
-          <button className="menu-open-btn" data-menu-open-btn>
-            {/* <ion-icon name="reorder-two"></ion-icon> */}
+          <button onClick={handleOnClickActive} className="menu-open-btn" data-menu-open-btn>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
           </button>
     
-          <nav className="navbar" data-navbar>
+          <nav className={`navbar ${isActive}`} data-navbar>
     
             <div className="navbar-top">
     
-              <a href="./index.html" className="logo">
-                <img src="./assets/images/logo.svg" alt="Filmlane logo"/>
-              </a>
+              <Link onClick={handleOnCloseSideNavbar} className="navbar-top-logo" href={"/"}>
+                <Image width={200} height={200} alt="Logo Arg Media play" src={"/loaderlogo.png"} ></Image>
+              </Link>
     
-              <button className="menu-close-btn" data-menu-close-btn>
-                {/* <ion-icon name="close-outline"></ion-icon> */}
+              <button onClick={handleOnCloseSideNavbar} className="menu-close-btn" data-menu-close-btn>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
               </button>
     
+            </div>
+
+            
+            <div className="actions-navbar-side">
+              <button className="search-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+              </button>
+      
+              <Link className={styles.button} href="#">
+                <span className={styles.button__iconwrapper}>
+                    <svg width="10" className={styles.button__iconsvg} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+                        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                    </svg>
+                    
+                    <svg className={`${styles.button__iconsvg}  ${styles.button__iconsvgcopy}`} xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+                        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+                    </svg>
+                </span>
+                Explore All
+              </Link>
             </div>
     
             <ul className="navbar-list">
     
               <li>
-                <a href="./index.html" className="navbar-link">Home</a>
+                 <Link onClick={handleOnCloseSideNavbar} className="navbar-link" href={""}>
+                    Homne
+                 </Link>
               </li>
     
               <li>
-                <a href="#" className="navbar-link">Movie</a>
+                 <Link onClick={handleOnCloseSideNavbar} className="navbar-link" href={""}>
+                  Categorias
+                 </Link>
               </li>
     
               <li>
-                <a href="#" className="navbar-link">Tv Show</a>
-              </li>
-    
-              <li>
-                <a href="#" className="navbar-link">Web Series</a>
-              </li>
-    
-              <li>
-                <a href="#" className="navbar-link">Pricing</a>
+                <Link onClick={handleOnCloseSideNavbar} className="navbar-link" href={""}>
+                  Beneficios
+                </Link>
               </li>
     
             </ul>
-    
-            <ul className="navbar-social-list">
-    
-              <li>
-                <a href="#" className="navbar-social-link">
-                  {/* <ion-icon name="logo-twitter"></ion-icon> */}
-                </a>
-              </li>
-    
-              <li>
-                <a href="#" className="navbar-social-link">
-                  {/* <ion-icon name="logo-facebook"></ion-icon> */}
-                </a>
-              </li>
-    
-              <li> 
-                <a href="#" className="navbar-social-link">
-                  {/* <ion-icon name="logo-pinterest"></ion-icon> */}
-                </a>
-              </li>
-    
-              <li>
-                <a href="#" className="navbar-social-link">
-                  {/* <ion-icon name="logo-instagram"></ion-icon> */}
-                </a>
-              </li>
-    
-              <li>
-                <a href="#" className="navbar-social-link">
-                  {/* <ion-icon name="logo-youtube"></ion-icon> */}
-                </a>
-              </li>
-    
-            </ul>
+
     
           </nav>
     
